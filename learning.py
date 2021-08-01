730ea5df38e2d4bae555f808d7defa7c554b82d4
Amit Bhattacharyya <amit@example.com>
2021-07-31 21:09:43 -0500

    cross entropy

---
+++ learning.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def cross_entropy(Y, P):
+    print(Y)
+    print(P)
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+
+print(cross_entropy([1, 1], [0.4, 0.5]))
+
+
